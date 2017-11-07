237d3cd84efa80a8d22b3c31d4a3587db84ad389
yutainoue <inoue_yuta@nexway.co.jp>
2017-11-07 23:11:42 +0900

    seedに演奏者名を入れ忘れていた

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 10.times do |no|
   ConcertInfo.create(
     concert_name:        "演奏会その#{no}",
+    performer_name:      "オーケストラその#{no}",
     hall:                "ホール名その#{no}",
     tact_down_time:      Time.current,
     opening_time:        Time.current,
     conductor:           "山田 #{no}太郎",
-    music_titles:        "ほにゃらら作曲 交響曲#{no}1番<br>ほにゃらら作曲 交響曲#{no}2番<br>ほにゃらら作曲 交響曲#{no}3番",
+    music_titles:        "ほにゃらら作曲 交響曲#{no}1番<br>
+                          ほにゃらら作曲 交響曲#{no}2番<br>
+                          ほにゃらら作曲 交響曲#{no}3番",
     contact_information: "einek#{no}@example.com",
     url:                 "http://concert-#{no}"
   )
