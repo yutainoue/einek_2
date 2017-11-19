1a12e8a3a81e4b56036f5c347f0ae8e971bc3824
yutainoue <inoue_yuta@nexway.co.jp>
2017-11-19 22:51:58 +0900

    テスト修正

--- spec/factories/concert_info.rb
+++ spec/factories/concert_info.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :concert_info do
-    concert_name        'ほにゃらら演奏会'
-    performer_name      'ほにゃらら楽団'
+    concert             'ほにゃらら演奏会'
+    performer           'ほにゃらら楽団'
     hall                'ほにゃららホール'
-    tact_down_time      Time.current
     opening_time        Time.current
     conductor           'ほにゃららさん'
     music_titles        '交響曲ほにゃらら'
     contact_information 'xxx@example.com'
-    url                 'http://xxx'
+    performer_url       'http://xxx'
+    page_url            'http://yyy'
   end
 end
