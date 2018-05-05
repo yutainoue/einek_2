4989955e7d00cedd1de09fab8619f2342dd62271
yutainoue <inoue_yuta@nexway.co.jp>
2018-05-05 15:18:47 +0900

    一覧画面をそのままダウンロードできるようにした

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -3,12 +3,16 @@ class ExportsController < ApplicationController
     @concert_infos = ConcertInfo.unexpired.where(id: params[:concert_info_ids].to_a).order('tactdown_time ASC')
 
     respond_to do |format|
+      format.csv do
+        send_data(Export.new.export_csv(@concert_infos), type: 'csv', filename: '挟み込みリスト.csv')
+      end
+
       format.xlsx do
         response.headers['Content-Disposition'] = 'attachment; filename="挟み込みリスト.xlsx"'
       end
 
       format.text do
-        send_data(Export.new.export_text(@concert_infos), type: 'text', filename: 'test.txt')
+        send_data(Export.new.export_text(@concert_infos), type: 'text', filename: '挟み込みリスト.txt')
       end
     end
   end
--- app/models/export.rb
+++ app/models/export.rb
@@ -1,22 +1,45 @@
 class Export
+  require 'csv'
+
   def export_text(concert_infos)
     text = ''
 
     concert_infos.each.with_index(1) do |c, i|
       text.concat <<~EOS
         =========================
-        #{i},#{c.concert}
+        #{i}. #{c.concert}
         　演奏者名：#{performer(c)}
-        　楽団ＨＰ：#{c.performer_url}
-        　開演時間：#{c.tactdown_time.strftime('%Y年%m月%d日 %H:%M')}
+        　ホール　：#{c.hall} (#{c.hall_prefecture}#{c.hall_ward})
         　開場時間：#{c.opening_time.strftime('%Y年%m月%d日 %H:%M')}
+        　開演時間：#{c.tactdown_time.strftime('%Y年%m月%d日 %H:%M')}
         　演奏曲目：#{music_titles(c)}
-        　ホール　：#{c.hall}
+        　楽団ＨＰ：#{c.performer_url}
         　備考　　：
       EOS
     end
 
     text << '========================='
+  end
+
+  def export_csv(concert_infos)
+    CSV.generate do |csv|
+      csv_header = %w[No. 演奏会名 ホール名 日程 曲目 HP]
+      csv << csv_header
+
+      concert_infos.each.with_index(1) do |c, i|
+        ActiveDecorator::Decorator.instance.decorate(c)
+
+        csv_column_values = [
+          i,
+          c.title,
+          c.hall_region,
+          c.schedule,
+          c.music_title,
+          c.performer_url
+        ]
+        csv << csv_column_values
+      end
+    end
   end
 
   private
